65d8588257223b0ca3d0c3565be6a2bc5212cce5
00008883 <wiut.00008883@gmail.com>
2020-11-18 14:35:57 +0500

    task1 modified

--- task1.py
+++ task1.py
@@ -1,15 +1,7 @@
-student1 = ('00008883', '19', 'Tashkent')
-student2 = ('00009623', '20', 'Andijan')
+student1 = ('00008883', 19, 'Tashkent')
+student2 = ('00009623', 18, 'Andijan')
 
-id1 = tuple(student1[0])
-id2 = tuple(student2[0])
 
-age1 = tuple(student1[1])
-age2 = tuple(student2[1])
-
-place1 = tuple(student1[2])
-place2 = tuple(student2[2])
-
-print(id1>id2)
-print(age1>age2)
-print(place1>place2)
+print(student1[0]>student2[0])
+print(student1[1]>student2[1])
+print(student1[2]>student2[2])
